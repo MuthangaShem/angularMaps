3bf3fead8163590887efb0ec5c379a09ae1a9bd3
muthangashem <muthangashem@gmail.com>
2019-04-18 23:56:06 +0300

    create script loading service and google maps component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GMapComponent } from './g-map/g-map.component';
+
+import { ScriptLoaderService } from './script-loader.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GMapComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ScriptLoaderService],
+  bootstrap: [AppComponent],
+  exports: [GMapComponent]
 })
 export class AppModule { }
---
+++ src/app/g-map/g-map.component.ts
@@ -0,0 +1,53 @@
+import { styledMap } from './../../assets/mapStyles/styledMap';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { ScriptLoaderService } from '../script-loader.service';
+import { HttpClient } from '@angular/common/http';
+
+const apiKey = 'AIzaSyAwVnwE1bEZf_Bkk_pSkGM0XlBSXJocVUY';
+const url = 'https://maps.googleapis.com/maps/api/js?key=' + apiKey + '&libraries=visualization';
+
+
+@Component({
+  selector: 'app-g-map',
+  templateUrl: './g-map.component.html',
+  styleUrls: ['./g-map.component.css'],
+})
+export class GMapComponent implements AfterViewInit {
+
+  @ViewChild('mapElement') mapElm: ElementRef;
+
+  private maps: any;
+  private map: any;
+  private coords: any;
+
+  constructor(private load: ScriptLoaderService, private http: HttpClient) { }
+
+  ngAfterViewInit(): void {
+    this.load.loadScript(url, 'gmap', () => {
+      this.maps = window[`google`][`maps`];
+      console.log(this.maps);
+      const loc = new this.maps.LatLng(51.561638, -0.14);
+
+      const darkmap = new this.maps.StyledMapType(styledMap, { name: 'Dark Map' });
+
+      this.coords = function (x, y) {
+        return new this.maps.LatLng(x, y);
+      };
+
+      this.map = new this.maps.Map(this.mapElm.nativeElement, {
+        zoom: 11,
+        center: loc,
+        scrollwheel: true,
+        panControl: false,
+        mapTypeControl: false,
+        zoomControl: true,
+        streetViewControl: false,
+        scaleControl: true,
+        zoomControlOptions: {
+          style: this.maps.ZoomControlStyle.LARGE,
+          position: this.maps.ControlPosition.RIGHT_BOTTOM
+        }
+      });
+    });
+  }
+}
