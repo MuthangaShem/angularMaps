b40133f84ede67f5a3d0c2e99068555957b35696
muthangashem <muthangashem@gmail.com>
2019-04-19 16:46:34 +0300

    display the map

--- src/app/ol-map/ol-map.component.ts
+++ src/app/ol-map/ol-map.component.ts
@@ -1,7 +1,7 @@
 import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { ScriptLoaderService } from '../script-loader.service';
 
-const url = 'https://openlayers.org/en/v4.6.4/build/ol.js';
+const url = 'https://openlayers.org/en/v4.6.5/build/ol.js';
 
 @Component({
   selector: 'app-ol-map',
@@ -10,9 +10,51 @@ const url = 'https://openlayers.org/en/v4.6.4/build/ol.js';
 })
 export class OlMapComponent implements AfterViewInit {
 
+  private stamen: any;
+  private osm: any;
+  private view: any;
+  private map: any;
+
   constructor(private load: ScriptLoaderService) { }
 
   ngAfterViewInit() {
+
+    this.load.loadScript(url, 'omap', () => {
+
+      const ol = window['ol'];
+
+      this.osm = new ol.layer.Tile({
+        source: new ol.source.OSM()
+      });
+
+      this.view = new ol.View({
+        center: ol.proj.fromLonLat([36.81667, -1.3]),
+        zoom: 10
+      });
+
+      const source = new ol.source.Vector({ wrapX: false });
+
+
+      this.stamen = new ol.layer.Tile({
+        source: new ol.source.Stamen({
+          layer: 'toner'
+        })
+      });
+
+
+      const vector = new ol.layer.Vector({
+        source: source
+      });
+
+      this.map = new ol.Map({
+        target: 'olmap',
+        layers: [this.osm, vector],
+        view: this.view
+      });
+
+
+    });
+
   }
 
 }
