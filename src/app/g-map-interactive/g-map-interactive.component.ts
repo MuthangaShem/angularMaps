e1c27847e0deb2d0486a1620fd90eb2fb1d82f61
muthangashem <muthangashem@gmail.com>
2019-04-19 15:05:00 +0300

    implement draw my face case

--- src/app/g-map-interactive/g-map-interactive.component.ts
+++ src/app/g-map-interactive/g-map-interactive.component.ts
@@ -17,8 +17,45 @@ export class GMapInteractiveComponent implements AfterViewInit {
 
   private map: any;
   private maps: any;
+  private drawingManager: any;
 
   constructor(private load: ScriptLoaderService) { }
+
+  draw(type) {
+    const maps = window['google']['maps'];
+    switch (type) {
+      case 'polygon':
+        this.drawingManager.setDrawingMode(maps.drawing.OverlayType.POLYGON);
+        this.drawingManager.setOptions({
+          polygonOptions: {
+            fillColor: '#9c4d4f',
+            fillOpacity: 0.5,
+            strokeWeight: 2,
+            strokeColor: '#401619',
+            clickable: true,
+            editable: true,
+            draggable: true
+          }
+        });
+        break;
+      case 'me':
+        this.drawingManager.setDrawingMode(maps.drawing.OverlayType.MARKER);
+        let me = new maps.MarkerImage('assets/me.png',
+          null,
+          null,
+          null,
+          new maps.Size(70, 70)
+        );
+        this.drawingManager.setOptions({
+          markerOptions: {
+            icon: me,
+            clickable: true,
+            draggable: true
+          }
+        });
+        break;
+    }
+  }
 
   ngAfterViewInit(): void {
     this.load.loadScript(url, 'gmap', () => {
@@ -44,6 +81,27 @@ export class GMapInteractiveComponent implements AfterViewInit {
       const drawControl = document.getElementById('draw-buttons');
       this.map.controls[maps.ControlPosition.TOP_LEFT].push(drawControl);
 
+      this.drawingManager = new maps.drawing.DrawingManager({
+        drawingMode: null,
+        drawingControl: false  // i have my custom tools so i don't need the defaults to be displayed
+      });
+      this.drawingManager.setMap(this.map);
+
+      maps.event.addListener(this.drawingManager, 'overlaycomplete', function (event) {
+        // console.log(event.type);
+        event.overlay.addListener('rightclick', function () {
+          event.overlay.setMap(null);
+        });
+        switch (event.type) {
+          case 'polygon':
+            this.map.data.add(new maps.Data.Feature({
+              geometry: new maps.Data.Polygon([event.overlay.getPath().getArray()])
+            }));
+            break;
+          default:
+            console.log('end');
+        }
+      });
     });
   }
 
