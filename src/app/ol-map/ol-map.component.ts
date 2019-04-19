9bbc2b94e64d54c24bf9318a60ab90a567308928
muthangashem <muthangashem@gmail.com>
2019-04-19 16:30:31 +0300

    add open layers url and import script loader

--- src/app/ol-map/ol-map.component.ts
+++ src/app/ol-map/ol-map.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { ScriptLoaderService } from '../script-loader.service';
+
+const url = 'https://openlayers.org/en/v4.6.4/build/ol.js';
 
 @Component({
   selector: 'app-ol-map',
   templateUrl: './ol-map.component.html',
   styleUrls: ['./ol-map.component.css']
 })
-export class OlMapComponent implements OnInit {
+export class OlMapComponent implements AfterViewInit {
 
-  constructor() { }
+  constructor(private load: ScriptLoaderService) { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
   }
 
 }
